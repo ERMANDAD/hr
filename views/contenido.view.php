f1a2a60eb84f5b341a8543381bfa5accacb7c73b
hernando rios <hernandorios69@gmail.com>
2023-08-12 22:39:36 -0500

    primer commit del repositorio

---
+++ views/contenido.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+    <h1 class="titulo">
+    LA VOZ DEL PUEBLO CENTRO INTEGRAL
+    </h1>
+    </div>
+    <a href="cerrar.php"></a>
+    <hr class="border">
+    <div class="contenido">
+        <article>
+            <p>INFORMACIÓN DE LA INSTITUCIÓN LA VOZ DEL PUEBLO CENTRO INTEGRAL Ubicado en la carrera séptima Nº 11-17 barrio Chapal en la ciudad de Pasto, Nariño Colombia de campo de acción privado con área en el departamento de Nariño dirección electrónica lavozdelpueblopasto@hotmail.com;  dirección general del centro integral LA VOZ DEL PUEBLO, Mg, Lucy Saldaña Bravo, número de contacto 3003801130 correo lucysalda1970@gmail.com.
+
+</p>
+            <p>LA VOZ DEL PUEBLO CENTRO INTEGRAL   Es una Organización No Gubernamental, sin ánimo de lucro, apolítica, sin distingo de razas, religión o condición social,  que como tal se regirá por el derecho privado, la Constitución Política Nacional, Decretos 1529 de 1990, Decreto 2150 de 1995, Decreto 0427 de 1996 y demás Normas legales vigentes, de  utilidad común, vigilada con lo dispuesto en la Normas de Ley vigentes.
+Desarrollar, implementar y dinamizar programas de capacitación y formación técnica para el trabajo para el beneficio de las personas adultos mayores, madres cabezas de familia de la zona rural.</p>
+        </article>
+    </div>
+
+</body>
+</html>
